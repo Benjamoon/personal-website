aa64b5a24c4585b0d1b0cf9fb3e310c882b162d1
Ben Lawrence <me@benlawrence.me>
2023-02-24 17:50:39 +0000

    Puuush!

---
+++ src/_data/projects.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        title: "This site (and all other projects)",
+        description: "All projects (including this site) are hosted using managed docker containers and are all configured behind a reverse proxy...",
+        link: "https://benlawrence.me"
+    },
+    {
+        title: "Chat application",
+        description: "Simple chat app using pocketbase auth and realtime data... Isnt the prettiest (no styling whatsover)",
+        link: "https://chat.benlawrence.me"
+    }
+]
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,10 @@
 module.exports = {
-	"title": "Eleventy Web Starter",
-	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
-	"url": "https://eleventywebstarter.netlify.app",
-	"author": "Chris Collins",
+	"title": "BenLawrence.me",
+	"description": "Not much of a website yet... See some public projects below... (They aren't that impressive)",
+	"url": "https://benlawrence.me",
+	"author": "Ben Lawrence",
 	"meta_data": {
-		"twitter": "@chrissy_dev",
+		"twitter": "@ben_lawrence1",
 		"opengraph_default": "/static/opengraph-default.jpg"
 	},
 	"env": process.env.ELEVENTY_ENV === 'production'
